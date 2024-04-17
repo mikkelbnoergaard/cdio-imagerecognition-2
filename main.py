fe7179b3cd379ffbb3deb645aedc37fdd5ee5a0e
Mikkel Nørgaard <mikkel.brunstedt@hotmail.dk>
2024-04-17 08:54:52 +0200

    test

--- main.py
+++ main.py
@@ -50,6 +50,7 @@ while True:
     clientsocket.close()
 
 
+
 # This program requires LEGO EV3 MicroPython v2.0 or higher.
 # Click "Open user guide" on the EV3 extension tab for more information.
 
