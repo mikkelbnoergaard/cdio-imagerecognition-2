5b050c984cec559b5ef29c5eeed95e4204a02d5b
Mikkel Nørgaard <mikkel.brunstedt@hotmail.dk>
2024-06-17 14:12:52 +0200

    mange opdateringer

--- connection.py
+++ connection.py
@@ -49,6 +49,8 @@ serversocket.bind((host, port))
 # Queue up to 5 requests
 serversocket.listen(5)
 
+eatBalls = True
+
 while True:
     # Establish a connection
     clientsocket,addr = serversocket.accept()
@@ -56,11 +58,12 @@ while True:
     # Receive the command
     command = clientsocket.recv(1024).decode('utf-8')
 
-    test_arm.run(-300)
+    if eatBalls:
+        test_arm.run(-1000)
     
     if command == 'FORWARD':
-        left_wheel.run(300)
-        right_wheel.run(300)
+        left_wheel.run(200)
+        right_wheel.run(200)
         time.sleep(0.2)
         left_wheel.stop()
         right_wheel.stop()
@@ -83,16 +86,37 @@ while True:
         right_wheel.stop()
         left_wheel.stop()
     elif command == 'ARM_IN':
-        test_arm.run(-310)
-        time.sleep(1.25)
-        test_arm.stop()
+        right_wheel.run(-200)
+        left_wheel.run(-200)
+        time.sleep(0.2)
+        right_wheel.stop()
+        left_wheel.stop()
     elif command == 'ARM_OUT':
-        test_arm.run(2000)
-        time.sleep(10)
+        left_wheel.stop()
+        right_wheel.stop()
         test_arm.stop()
+        time.sleep(0.5)
+        eatBalls = False
+        test_arm.run(5000)
     elif command == 'STOP':
         left_wheel.stop()  # Stop the left wheel
         right_wheel.stop()  # Stop the right wheel
         test_arm.stop()  # Stop the test arm
+    elif command == 'NAVIGATE_LEFT':
+        left_wheel.run(-200)
+        time.sleep(0.2)
+        left_wheel.run(100)
+        right_wheel.run(200)
+        time.sleep(0.2)
+        left_wheel.stop()
+        right_wheel.stop()
+    elif command == 'NAVIGATE_RIGHT':
+        right_wheel.run(-200)
+        time.sleep(0.2)
+        right_wheel.run(100)
+        left_wheel.run(200)
+        time.sleep(0.2)
+        right_wheel.stop()
+        left_wheel.stop()
 
     clientsocket.close()
